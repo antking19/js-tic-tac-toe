8a6c39ae48e55882668a7c56b880162be6467be1
cuongtk <cuong.truongkien@gmail.com>
2023-03-03 10:39:36 +0700

    finish tic tac toe game 03-03-2023

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 /**
  * Global variables
  */
-import { TURN } from "./constants.js";
-import { getCellElementList, getCurrentTurnElement } from "./selectors.js";
+import { TURN, CELL_VALUE, GAME_STATUS } from "./constants.js";
+import {
+    getCellElementList,
+    getCurrentTurnElement,
+    getGameStatusElement,
+    getReplayButton,
+    getCellElementAtIdx,
+} from "./selectors.js";
+import { checkGameStatus } from "./utils.js";
 
 let currentTurn = TURN.CROSS;
-let isGameEnded = false;
 let cellValues = new Array(9).fill("");
+let gameStatus = GAME_STATUS.PLAYING;
 
 function toggleTurn() {
     // toggle turn
@@ -20,6 +27,27 @@ function toggleTurn() {
     }
 }
 
+function updateGameStatus(newGameStatus) {
+    gameStatus = newGameStatus;
+    const gameStatusElement = getGameStatusElement();
+    gameStatusElement.textContent = newGameStatus;
+}
+
+function showReplayButton() {
+    const replayButton = getReplayButton();
+    if (replayButton) replayButton.classList.add("show");
+}
+
+function hightlightCellWinValues(winPositions) {
+    if (!Array.isArray(winPositions) || winPositions.length !== 3)
+        throw new Error("Invalid highlight win");
+
+    for (const position of winPositions) {
+        const cellElement = getCellElementAtIdx(position);
+        cellElement.classList.add("win");
+    }
+}
+
 function handleCellClick(cell, index) {
     const isClicked =
         cell.classList.contains(TURN.CIRCLE) ||
@@ -29,22 +57,89 @@ function handleCellClick(cell, index) {
     // set selected cell
     cell.classList.add(currentTurn);
 
+    cellValues[index] =
+        currentTurn === TURN.CROSS ? CELL_VALUE.CROSS : CELL_VALUE.CIRCLE;
+
+    const game = checkGameStatus(cellValues);
+
     // toggle turn
     toggleTurn();
 
-    console.log("click", cell, index);
+    switch (game.status) {
+        case GAME_STATUS.X_WIN:
+        case GAME_STATUS.O_WIN: {
+            updateGameStatus(game.status);
+            showReplayButton();
+            hightlightCellWinValues(game.winPositions);
+            break;
+        }
+
+        case GAME_STATUS.ENDED: {
+            updateGameStatus(game.status);
+            showReplayButton();
+            break;
+        }
+
+        default:
+    }
+}
+
+function resetGame() {
+    // reset gameStatus 'LOADING'
+    const gameStatusElement = getGameStatusElement();
+    if (gameStatusElement) {
+        gameStatusElement.textContent = "LOADING";
+    }
+
+    // reset all cell value element
+    // reset highlight win element
+    const cellElementList = getCellElementList();
+    if (cellElementList) {
+        for (const cellElement of cellElementList) {
+            cellElement.className = "";
+        }
+    }
+
+    // reset currentTurn 'X'
+    const currentTurnElement = getCurrentTurnElement();
+    if (currentTurnElement) {
+        currentTurnElement.classList.remove(TURN.CROSS, TURN.CIRCLE);
+        currentTurnElement.classList.add(TURN.CROSS);
+    }
+
+    // hide replay button
+    const replayButton = getReplayButton();
+    if (replayButton) {
+        replayButton.classList.remove("show");
+    }
+}
+
+function initResetGame() {
+    // get replay button
+    const replayButton = getReplayButton();
+    if (!replayButton) return;
+
+    replayButton.addEventListener("click", resetGame);
 }
 
 function initCellElementList() {
     const cellElementList = getCellElementList();
 
     cellElementList.forEach((cell, index) => {
-        cell.addEventListener("click", () => handleCellClick(cell, index));
+        cell.dataset.id = index;
+    });
+
+    const cellList = document.getElementById("cellList");
+    cellList.addEventListener("click", (event) => {
+        let target = event.target;
+        if (target.tagName !== "LI") return;
+        handleCellClick(target, Number.parseInt(target.dataset.id));
     });
 }
 
 (() => {
     initCellElementList();
+    initResetGame();
 })();
 
 /**
